7ef1e27ffecb6c473048ee8f87a8badae784715d
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-12-18 21:13:11 +0300

    initialize spring project

---
+++ fournisseur_identite/src/test/java/mg/working/fournisseur_identite/FournisseurIdentiteApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.working.fournisseur_identite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FournisseurIdentiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
