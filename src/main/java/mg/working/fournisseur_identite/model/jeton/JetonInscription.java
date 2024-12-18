1e5868a51cea1f932f64b9453d2de8577ec1ab63
Kaloina RANDRIAMBOLOLONA <143845374+KaloinaRandria@users.noreply.github.com>
2024-12-18 22:01:06 +0300

    Update JetonInscription.java

--- src/main/java/mg/working/fournisseur_identite/model/jeton/JetonInscription.java
+++ src/main/java/mg/working/fournisseur_identite/model/jeton/JetonInscription.java
@@ -1,5 +1,56 @@
 package mg.working.fournisseur_identite.model.jeton;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "jeton_inscription")
+@SequenceGenerator(
+    name = "s_jeton_inscription",
+    sequenceName = "s_jeton_inscription",
+    allocationSize = 1
+)
 public class JetonInscription {
+    @Id @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "s_jeton_inscription") @Column(name = "id_jeton_inscription")
+    int id;
+    String mail;
+    String mdp;
+    String nom;
+    LocalDate dateNaissance;
+    @ManyToOne @JoinColumn(name = "id_jeton" , referencedColumnName = "id_jeton")
+    @Column(unique = true)
+    Jeton jeton;
+
+     // Constructeur personnalisé
+    public JetonInscription(String mail, String mdp, String nom, LocalDate dateNaissance, Jeton jeton) {
+        this.mail = mail;
+        this.mdp = mdp;
+        this.nom = nom;
+        this.dateNaissance = dateNaissance;
+        this.jeton = jeton;
+    }
+
+    // Méthode pour vérifier si le jeton est expiré
+    public boolean isExpired() {
+        return jeton.getExpirationUtil().getDateExpiration().isBefore(LocalDateTime.now());
+    }
 
 }
